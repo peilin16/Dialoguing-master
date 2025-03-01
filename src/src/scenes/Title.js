77a676557e1419b7c9114e094809934a9fb88af1
peilin16 <105468929+peilin16@users.noreply.github.com>
2025-02-28 23:41:00 -0800

    Add files via upload

---
+++ src/src/scenes/Title.js
@@ -0,0 +1,40 @@
+class Title extends Phaser.Scene {
+    constructor() {
+        super("titleScene")
+    }
+
+    preload() {
+        // load assets
+        this.load.path = "./assets/"
+
+        // load JSON (ie dialog text)
+        this.load.json('dialog', 'json/dialog.json')
+
+        // load images
+        this.load.image('dialogbox', 'img/dialogbox.png')
+        this.load.image('homer', 'img/homer.png')
+        this.load.image('minerva', 'img/minerva.png')
+        this.load.image('jove', 'img/jove.png')
+        this.load.image('neptune', 'img/neptune.png')
+        this.load.image('rumia','img/rumia.png')//新加的
+        // load bitmap font
+        this.load.bitmapFont('gem_font', 'font/gem.png', 'font/gem.xml')
+        this.load.bitmapFont('gem2_font', 'font/gem2.png', 'font/gem2.xml')
+    }
+
+    create() {
+        // add title text
+        this.add.bitmapText(centerX, centerY - 32, 'gem2_font', 'THE ODYSSEY', 32).setOrigin(0.5)
+        this.add.bitmapText(centerX, centerY, 'gem2_font', 'Press SPACE to start', 16).setOrigin(0.5)
+
+        // create input
+        cursors = this.input.keyboard.createCursorKeys()
+    }
+
+    update() {
+        // wait for player input
+        if(Phaser.Input.Keyboard.JustDown(cursors.space)) {
+            this.scene.start("talkingScene")
+        }
+    }
+}
